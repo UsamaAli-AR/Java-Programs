0b7730b81a1b6b8537ed0f3da0611ce0e095ccd1
Usama ALi <uali24272@gmail.com>
2024-05-12 18:01:33 +0500

    Sum numbers using Method

---
+++ MethodSumDemo.java
@@ -0,0 +1,10 @@
+public class MethodSumDemo {
+
+    public static void sum(int a, int b) {
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(43, 55);
+    }
+}
